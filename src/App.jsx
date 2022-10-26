af0fd4317e991a145f4308d883f453ec40a06977
rossianllela <rossianllela@gmail.com>
2022-10-26 13:52:13 -0400

    PLAY LISTA

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import AsideMenu from './components/AsideMenu/AsideMenu';
 import Home from './components/Home/Home';
 import Trade from './components/Trade/Trade';
 import Farm from './components/Farm/Farm';
+import Play from './components/Play/Play';
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
 
 <div className='container'>
 <AsideMenu />
-      <Farm />
+      <Play />
 </div>
      
     </>
---
+++ src/components/Play/Play.jsx
@@ -0,0 +1,45 @@
+import './Play.css';
+
+function Play () {
+    return (
+        <>
+            <div className="home-container">
+            <div className="card-containers">
+                    <div className="cards">
+                        <div className="card-header">
+                        <center>
+                            <div className="card-header-text">
+                                <p>PLAY AND WIN</p>
+                                <br />
+                                <h2>Looking for REAL MONEY rewards? MONEY LAND is the right place to EARN MONEY.</h2>
+                            </div>
+                        </center>
+                            </div>
+                            </div>
+                            </div>      
+                            <div className="data-values">
+                    <div className="gradient-div">
+                        <div className="background-div">
+                            <h3>Flappy Bird</h3>
+
+                            <center>
+                                    <button class="redondo">GO</button>
+                                    </center>
+                        </div>
+                    </div>
+                    <div className="gradient-div">
+                        <div className="background-div">
+                            <h3>Tetris</h3>
+                            <center>
+                                    <button class="redondo">GO</button>
+                                    </center>
+                        </div>
+                    </div>
+                </div>
+                  
+            </div>
+        </>
+    );
+}
+
+export default Play;
