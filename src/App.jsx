e3ed8d7213850abf62f13d829e1be9ccb9e1089d
rossianllela <rossianllela@gmail.com>
2022-10-26 13:44:14 -0400

    LISTO PARTE DE FARM

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import AsideMenu from './components/AsideMenu/AsideMenu';
 import Home from './components/Home/Home';
 import Trade from './components/Trade/Trade';
+import Farm from './components/Farm/Farm';
 
 function App() {
 
@@ -10,7 +11,7 @@ function App() {
 
 <div className='container'>
 <AsideMenu />
-      <Trade />
+      <Farm />
 </div>
      
     </>
---
+++ src/components/Farm/Farm.jsx
@@ -0,0 +1,244 @@
+import './Farm.css';
+
+function Farm () {
+    return (
+        <>
+            <div className="home-container">
+            <div className="card-containers">
+                    <div className="cards">
+                        <div className="card-header">
+                        <center>
+                            <div className="card-header-text">
+                                <p>Money Farm</p>
+                                <br />
+                                <h2>Stake Liquidity Pool (LP) tokens to earn.</h2>
+                            </div>
+                        </center>
+                            </div>
+                            </div>
+                            </div>      
+                            <div className="card-containers">
+                    <div className="cards">
+                        <div className="card-header">
+                            <div className="card-header-text">
+                                <p>MONEY-BUSD</p>
+                            </div>
+                            <div className="footer-field-buttons">
+                                <button className="button-field-data">CORE</button>
+                                <button className="button-field-data">40X</button>
+                            </div>
+                        </div>
+                        <div className="card-body">
+                            <p>APR:</p><br />
+                            <p>0</p>
+                        </div>
+                        <div className="card-body">
+                            <p>EARN:</p><br />
+                            <p>0</p>
+                        </div>
+                        <div className="card-footer">
+                        <center><div className="footer-fields">
+                                <p>MONEY EARNED</p><br />
+                            </div>
+                        </center>
+                            <div className="footer-field-buttons">
+                                <button className="button-field-data">HARVEST</button>
+                            </div>
+                            <center><div className="footer-fields">
+                                <p>MONEY-BUSD LP STAKED</p><br />
+                            </div>
+                        </center>
+                            <div className="footer-field-buttons">
+                                <button className="button-field-data">HARVEST</button>
+                            </div>     
+                        </div>
+                    </div>
+
+                    <div className="cards">
+                        <div className="card-header">
+                            <div className="card-header-text">
+                                <p>MONEY-BNB</p>
+                            </div>
+                            <div className="footer-field-buttons">
+                                <button className="button-field-data">CORE</button>
+                                <button className="button-field-data">24X</button>
+                            </div>
+                        </div>
+                        <div className="card-body">
+                            <p>APR:</p><br />
+                            <p>0</p>
+                        </div>
+                        <div className="card-body">
+                            <p>EARN:</p><br />
+                            <p>0</p>
+                        </div>
+                        <div className="card-footer">
+                        <center><div className="footer-fields">
+                                <p>MONEY EARNED</p><br />
+                            </div>
+                        </center>
+                            <div className="footer-field-buttons">
+                                <button className="button-field-data">HARVEST</button>
+                            </div>
+                            <center><div className="footer-fields">
+                                <p>MONEY-BNB LP STAKED</p><br />
+                            </div>
+                        </center>
+                            <div className="footer-field-buttons">
+                                <button className="button-field-data">HARVEST</button>
+                            </div>     
+                        </div>
+                    </div>
+
+                    <div className="cards">
+                        <div className="card-header">
+                            <div className="card-header-text">
+                                <p>BUSD-BNB</p>
+                            </div>
+                            <div className="footer-field-buttons">
+                                <button className="button-field-data">CORE</button>
+                                <button className="button-field-data">1X</button>
+                            </div>
+                        </div>
+                        <div className="card-body">
+                            <p>APR:</p><br />
+                            <p>0</p>
+                        </div>
+                        <div className="card-body">
+                            <p>EARN:</p><br />
+                            <p>0</p>
+                        </div>
+                        <div className="card-footer">
+                        <center><div className="footer-fields">
+                                <p>MONEY EARNED</p><br />
+                            </div>
+                        </center>
+                            <div className="footer-field-buttons">
+                                <button className="button-field-data">HARVEST</button>
+                            </div>
+                            <center><div className="footer-fields">
+                                <p>BNB-BUSD LP STAKED</p><br />
+                            </div>
+                        </center>
+                            <div className="footer-field-buttons">
+                                <button className="button-field-data">HARVEST</button>
+                            </div>     
+                        </div>
+                    </div>                     
+                    </div>
+
+                    <div className="card-containers">
+                    <div className="cards">
+                        <div className="card-header">
+                            <div className="card-header-text">
+                                <p>USDT-BUSD</p>
+                            </div>
+                            <div className="footer-field-buttons">
+                                <button className="button-field-data">CORE</button>
+                                <button className="button-field-data">1X</button>
+                            </div>
+                        </div>
+                        <div className="card-body">
+                            <p>APR:</p><br />
+                            <p>0</p>
+                        </div>
+                        <div className="card-body">
+                            <p>EARN:</p><br />
+                            <p>0</p>
+                        </div>
+                        <div className="card-footer">
+                        <center><div className="footer-fields">
+                                <p>MONEY EARNED</p><br />
+                            </div>
+                        </center>
+                            <div className="footer-field-buttons">
+                                <button className="button-field-data">HARVEST</button>
+                            </div>
+                            <center><div className="footer-fields">
+                                <p>USDT-BUSD LP STAKED</p><br />
+                            </div>
+                        </center>
+                            <div className="footer-field-buttons">
+                                <button className="button-field-data">HARVEST</button>
+                            </div>     
+                        </div>
+                    </div>
+
+                    <div className="cards">
+                        <div className="card-header">
+                            <div className="card-header-text">
+                                <p>USDT-BNB</p>
+                            </div>
+                            <div className="footer-field-buttons">
+                                <button className="button-field-data">CORE</button>
+                                <button className="button-field-data">1X</button>
+                            </div>
+                        </div>
+                        <div className="card-body">
+                            <p>APR:</p><br />
+                            <p>0</p>
+                        </div>
+                        <div className="card-body">
+                            <p>EARN:</p><br />
+                            <p>0</p>
+                        </div>
+                        <div className="card-footer">
+                        <center><div className="footer-fields">
+                                <p>MONEY EARNED</p><br />
+                            </div>
+                        </center>
+                            <div className="footer-field-buttons">
+                                <button className="button-field-data">HARVEST</button>
+                            </div>
+                            <center><div className="footer-fields">
+                                <p>USDT-BNB LP STAKED</p><br />
+                            </div>
+                        </center>
+                            <div className="footer-field-buttons">
+                                <button className="button-field-data">HARVEST</button>
+                            </div>     
+                        </div>
+                    </div>
+
+                    <div className="cards">
+                        <div className="card-header">
+                            <div className="card-header-text">
+                                <p>BTCB-BNB</p>
+                            </div>
+                            <div className="footer-field-buttons">
+                                <button className="button-field-data">CORE</button>
+                                <button className="button-field-data">1X</button>
+                            </div>
+                        </div>
+                        <div className="card-body">
+                            <p>APR:</p><br />
+                            <p>0</p>
+                        </div>
+                        <div className="card-body">
+                            <p>EARN:</p><br />
+                            <p>0</p>
+                        </div>
+                        <div className="card-footer">
+                        <center><div className="footer-fields">
+                                <p>MONEY EARNED</p><br />
+                            </div>
+                        </center>
+                            <div className="footer-field-buttons">
+                                <button className="button-field-data">HARVEST</button>
+                            </div>
+                            <center><div className="footer-fields">
+                                <p>BTCB-BNB LP STAKED</p><br />
+                            </div>
+                        </center>
+                            <div className="footer-field-buttons">
+                                <button className="button-field-data">HARVEST</button>
+                            </div>     
+                        </div>
+                    </div>                     
+                    </div>                    
+            </div>
+        </>
+    );
+}
+
+export default Farm;
