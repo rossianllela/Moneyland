8e0bc57cf5d23242e8498627232e0ccc40983380
luismimichel <88723468+luismimichel@users.noreply.github.com>
2022-10-29 12:29:07 -0400

    menu

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,15 @@ import Header from './components/Header/Header';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import { store } from  './state/store'
+import Home from './components/Home/Home';
+import Trade from './components/Trade/Trade';
+import Farm from './components/Farm/Farm';
+import AsideMenu from './components/AsideMenu/AsideMenu';
+import Play from './components/Play/Play';
+import Lottery from './components/Lottery/Lottery';
+import Nft from './components/Nft/Nft';
+import Invited from './components/Invited/Invited';
+import Perfil from './components/Perfil/Perfil';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -13,8 +22,18 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
     <BrowserRouter>
     <Header />
+    <AsideMenu />
       <Routes>
-        <Route path={ '/' } element={ <App /> } />
+        <Route path={ '/' } element={ <Home/> } />
+        <Route path={ 'Trade' } element={ <Trade /> } />
+        <Route path={ 'Farm' } element={ <Farm /> } />
+        <Route path={ 'Banks' } element={ <Farm /> } />
+        <Route path={ 'Game' } element={ <Play /> } />
+        <Route path={ 'Lottery' } element={ <Lottery /> } />
+        <Route path={ 'marketplace' } element={ <Nft /> } />
+        <Route path={ 'Invite' } element={ <Invited /> } />
+        <Route path={ 'Perfil' } element={ <Perfil/> } />
+
       </Routes>
     </BrowserRouter>
     </Provider>
