cecf85094425bdd2a9d7af2c6be4e5fa21811821
rossianllela <112584537+rossianllela@users.noreply.github.com>
2022-10-29 13:41:50 -0400

    juego

--- src/components/Play/Play.jsx
+++ src/components/Play/Play.jsx
@@ -21,6 +21,7 @@ function Play () {
                     <div className="gradient-div">
                         <div className="background-div">
                             <h3>Flappy Bird</h3>
+                            <img class="circular--square-play" src="https://i.pinimg.com/originals/a0/10/96/a01096406d987a54c14d498a6b420960.png" />
 
                             <center>
                                     <button class="redondo">GO</button>
@@ -30,6 +31,7 @@ function Play () {
                     <div className="gradient-div">
                         <div className="background-div">
                             <h3>Tetris</h3>
+                            <img class="circular--square-play" src="https://i.pinimg.com/favicons/993428f57895a5cfb7f1de1fb48ed2d1ab691b4ecf35615fab8f27fb.png?b0b6776fa66c2f538ac70668611840ea" />
                             <center>
                                     <button class="redondo">GO</button>
                                     </center>
